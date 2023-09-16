58cc989f20a39086774b35d5a173d29163c82e9d
Omar Yasser <omar_yasser@Omars-MacBook-Air.local>
2023-09-16 19:54:29 +0300

    Feat: add wireframe for milestone 4

--- src/data/cooking.py
+++ src/data/cooking.py
@@ -9,13 +9,18 @@ def init_tensor(arr: np.array) -> tuple[np.array, np.array]:
 def main():
     """
     """
+    ### load data ###
     tr = pd.read_csv("./dbs/cooked/tr.csv")
     ts = pd.read_csv("./dbs/cooked/ts.csv")
     
+    ### convert to numpy ###
     tr = tr.to_numpy()
     ts = ts.to_numpy()
 
+    ### init tensors ###
     tr_X, tr_y = init_tensor(tr)
     ts_X, ts_y = init_tensor(ts)
+
+    ### save tensors in `dbs/cooked/` ###
 
 if (__name__ == "__main__"):    main()
